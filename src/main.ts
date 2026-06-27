cb1c917b676a6c7e46416c62359903d830aa0084
Okoronkwo Nnamdi Paschal <paschalnnamdi54@gmail.com>
2026-06-27 19:47:43 +0100

    fix: update CORS for production

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,14 @@ from './shared/filters/global-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://eventful-ui-dusky.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api/v1');
 
